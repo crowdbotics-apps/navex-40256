04ec79ba2bd82f5747bc1fbce4f06a4406cacbfc
DOMINICK DADDIO <dominick@crowdbotics.com>
2023-04-27 12:12:14 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "navex-40256",
-  url: "https://navex-40256.botics.co",
-  api: "https://navex-40256.botics.co/api/v1"
-}
+  "name": "navex-40256",
+  "url": "https://navex-40256.botics.co",
+  "api": "https://navex-40256.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
